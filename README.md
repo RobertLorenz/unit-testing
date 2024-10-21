967de382697250e7410436e9fea9f1b2c4fc0326
Róbert Lőrinc <robert.lorinc@casrd.net>
2024-10-21 14:24:28 +0200

    demo setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unit-testing
+
+### Project setup
+
+ 1. Clone the repository :
+
+ `git clone https://github.com/RobertLorenz/unit-testing.git`
+
+ 2. Run the following command:
+
+`npm install`
+
+ 3. Run tests with `npm test` command
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 module.exports = function () {
-    this.add = function (a, b) {
-      return a + b;
-    };
-  
-    this.multiply = function (a, b) {
-      return a * b;
-    };
-  
-    this.fibonacci = function (n) {
-      if (n <= 1) {
-        return n;
-      }
-      return this.fibonacci(n - 1) + this.fibonacci(n - 2);
-    };
+  this.add = function (a, b) {
+    return a + b;
   };
-  
+
+  this.multiply = function (a, b) {
+    return a * b;
+  };
+
+  this.fibonacci = function (n) {
+    if (typeof n !== "number" || n < 0) {
+      return -1;
+    }
+    if (n <= 1) {
+      return n;
+    }
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  };
+};
